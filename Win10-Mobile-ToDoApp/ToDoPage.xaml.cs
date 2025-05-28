8f89ef56573841d7a7d8904d3a5b0ec534952627
John <john.koshy@outlook.com>
2025-05-28 08:26:51 +0530

    Fixed strike-through by adding INotifyPropertyChanged

--- Win10-Mobile-ToDoApp/ToDoPage.xaml.cs
+++ Win10-Mobile-ToDoApp/ToDoPage.xaml.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 namespace Win10_Mobile_ToDoApp
 {
-    public class TaskItem
+    public class TaskItem : INotifyPropertyChanged
     {
-        public string TaskName { get; set; }
-        public bool IsCompleted { get; set; }
+        private string _taskName;
+        private bool _isCompleted;
+
+        public string TaskName
+        {
+            get => _taskName;
+            set
+            {
+                _taskName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                _isCompleted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class BoolToTextDecorationsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool isCompleted && isCompleted)
+            {
+                return TextDecorations.Strikethrough;
+            }
+            return TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public sealed partial class ToDoPage : Page
